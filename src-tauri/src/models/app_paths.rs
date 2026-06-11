1a5d5403f4240e2e9f1848a360b12ed27f0f3277
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-11 22:16:09 +0800

    feat: add app paths model

---
+++ src-tauri/src/models/app_paths.rs
@@ -0,0 +1,9 @@
+use std::path::PathBuf;
+
+pub struct AppPaths {
+    pub root: PathBuf,
+    pub database: PathBuf,
+    pub config: PathBuf,
+    pub attachments: PathBuf,
+    pub cache: PathBuf,
+}
