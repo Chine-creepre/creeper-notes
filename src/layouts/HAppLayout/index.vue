aabd73ebd539c42b6f25ad0c22e6a0ae49bc8efb
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-11 20:56:01 +0800

    feat: add custom window title bar

---
+++ src/layouts/HAppLayout/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import HWindowTitleBar from "@/components/HWindowTitleBar/index.vue";
+import { useHAppLayout } from "./hook";
+
+const {} = useHAppLayout();
+</script>
+
+<template>
+  <div class="h_app_layout">
+    <HWindowTitleBar />
+
+    <main class="h_app_layout_content">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped src="./index.scss"></style>
