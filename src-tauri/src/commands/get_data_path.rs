5576c8837cd748bcfb12c2cfc92983c6f45f44a3
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-11 22:20:33 +0800

    fix: return app data root path

--- src-tauri/src/commands/get_data_path.rs
+++ src-tauri/src/commands/get_data_path.rs
@@ -2,7 +2,7 @@ use crate::services::path_service;
 
 #[tauri::command]
 pub fn get_data_path() -> Result<String, String> {
-    let app_data_path = path_service::get_app_data_path()?;
+    let app_paths = path_service::get_app_data_path()?;
 
-    Ok(app_data_path.to_string_lossy().to_string())
+    Ok(app_paths.root_dir.to_string_lossy().to_string())
 }
