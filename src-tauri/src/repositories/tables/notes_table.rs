d68ddc475a59011c8c1388fe41a17926af464508
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-11 23:24:48 +0800

    refactor: use notes table constant

--- src-tauri/src/repositories/tables/notes_table.rs
+++ src-tauri/src/repositories/tables/notes_table.rs
@@ -1,21 +1,26 @@
 use rusqlite::Connection;
 
+use crate::constants::note_constants::NOTES_TABLE_NAME;
+
 pub fn initialize(connection: &Connection) -> Result<(), String> {
+    let sql = format!(
+        r#"
+        CREATE TABLE IF NOT EXISTS {} (
+            id TEXT PRIMARY KEY,
+            title TEXT NOT NULL,
+            describe TEXT,
+            content TEXT,
+            readonly INTEGER NOT NULL DEFAULT 0,
+            created_at INTEGER NOT NULL,
+            updated_at INTEGER NOT NULL,
+            deleted INTEGER NOT NULL DEFAULT 0
+        );
+        "#,
+        NOTES_TABLE_NAME,
+    );
+
     connection
-        .execute_batch(
-            r#"
-            CREATE TABLE IF NOT EXISTS notes (
-                id TEXT PRIMARY KEY,
-                title TEXT NOT NULL,
-                describe TEXT,
-                content TEXT,
-                readonly INTEGER NOT NULL DEFAULT 0,
-                created_at INTEGER NOT NULL,
-                updated_at INTEGER NOT NULL,
-                deleted INTEGER NOT NULL DEFAULT 0
-            );
-            "#,
-        )
+        .execute_batch(&sql)
         .map_err(|error| error.to_string())?;
 
     Ok(())
