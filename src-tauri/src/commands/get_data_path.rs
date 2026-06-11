5bcda3e2711635c47cedf79b6c4bd3b8f749452e
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-11 22:24:37 +0800

    fix: pass app handle to path service

--- src-tauri/src/commands/get_data_path.rs
+++ src-tauri/src/commands/get_data_path.rs
@@ -1,8 +1,8 @@
 use crate::services::path_service;
 
 #[tauri::command]
-pub fn get_data_path() -> Result<String, String> {
-    let app_paths = path_service::get_app_data_path()?;
+pub fn get_data_path(app: tauri::AppHandle) -> Result<String, String> {
+    let app_paths = path_service::get_app_data_path(&app)?;
 
     Ok(app_paths.root_dir.to_string_lossy().to_string())
 }
