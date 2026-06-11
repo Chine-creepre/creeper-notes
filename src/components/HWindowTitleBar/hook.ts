1b7381459075cf2f9bb6cdb399603c4326972e1a
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-11 21:06:09 +0800

    fix: start dragging window manually

--- src/components/HWindowTitleBar/hook.ts
+++ src/components/HWindowTitleBar/hook.ts
@@ -1,5 +1,13 @@
+import { getCurrentWindow } from "@tauri-apps/api/window";
+
 export const useHWindowTitleBar = () => {
+  const appWindow = getCurrentWindow();
+
+  const startDragWindow = async (): Promise<void> => {
+    await appWindow.startDragging();
+  };
 
   return {
+    startDragWindow,
   };
 };
