76cb3022e36b819b37c659b5ef1456d2d2a23d11
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-11 20:11:26 +0800

    feat: initialize router and pinia

---
+++ src/views/Bootstrap.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="bootstrap-page">
+    creeper-notes
+  </div>
+</template>
+
+<style scoped>
+.bootstrap-page {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
