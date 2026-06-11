a55429286491a8df469a35dcd6b074883fef4103
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-11 21:09:11 +0800

    fix: bind manual window dragging

--- src/components/HWindowTitleBar/index.vue
+++ src/components/HWindowTitleBar/index.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { useHWindowTitleBar } from "./hook";
 
-const {} = useHWindowTitleBar();
+const { startDragWindow } = useHWindowTitleBar();
 </script>
 
 <template>
-  <header
-    class="h_window_title_bar"
-    data-tauri-drag-region>
-    <div class="h_window_title_bar_drag">
-      <span class="h_window_title_bar_title"> creeper-notes </span>
+  <header class="h_window_title_bar">
+    <div class="h_window_title_bar_drag" @mousedown.left="startDragWindow">
+      <span class="h_window_title_bar_title">
+        creeper-notes
+      </span>
     </div>
   </header>
 </template>
