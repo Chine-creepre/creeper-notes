d5cc3c1ced919d27586d4abb11d3c4903f89d265
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-11 20:26:24 +0800

    refactor: align bootstrap page structure

---
+++ src/views/Bootstrap/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useBootstrap } from "./hook";
+
+const {} = useBootstrap();
+</script>
+
+<template>
+  <div class="h_bootstrap">
+    creeper-notes
+  </div>
+</template>
+
+<style lang="scss" scoped src="./index.scss"></style>
