d622ef424a14fb6234302dd5b1ad6901f2e90efb
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-15 19:55:23 +0800

    feat: add app config commands

---
+++ src-tauri/src/commands/config_commands.rs
@@ -0,0 +1,14 @@
+use tauri::AppHandle;
+
+use crate::models::app_config::AppConfig;
+use crate::services::config_service;
+
+#[tauri::command]
+pub fn get_config(app: AppHandle) -> Result<AppConfig, String> {
+    config_service::get_config(&app)
+}
+
+#[tauri::command]
+pub fn update_config(app: AppHandle, app_config: AppConfig) -> Result<AppConfig, String> {
+    config_service::update_config(&app, app_config)
+}
