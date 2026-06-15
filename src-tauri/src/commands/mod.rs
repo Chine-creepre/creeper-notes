7ea8924ec6fa328ca4ace2f55712eac0e6f9abd3
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-15 19:42:57 +0800

    feat: export note commands

--- src-tauri/src/commands/mod.rs
+++ src-tauri/src/commands/mod.rs
@@ -1,5 +1,14 @@
 mod get_data_path;
+mod note_commands;
 mod start_dragging_window;
 
 pub use get_data_path::get_data_path;
+pub use note_commands::{
+    create_note,
+    delete_note,
+    find_note_by_id,
+    list_notes,
+    search_notes,
+    update_note,
+};
 pub use start_dragging_window::start_dragging_window;
