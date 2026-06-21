515282d787b4651648f7e2175fa10f3702b2be7c
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-21 17:00:07 +0800

    refactor: remove quick search header area

--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -1,77 +1,59 @@
 <template>
   <div class="h_search">
     <section class="h_search_panel">
-      <header class="h_search_header" @mousedown.left="startDragWindow">
-        <div class="h_search_brand">
-          <span class="h_search_brand_icon">
-            <Icon icon="lucide:search" />
-          </span>
-          <div>
-            <h1>快速搜索</h1>
-            <p>搜索笔记、想法和待办</p>
-          </div>
-        </div>
-
-        <div class="h_search_header_actions" @mousedown.stop>
-          <button class="h_search_close" type="button" @click.stop="closeSearch">
-            <Icon icon="lucide:x" />
-          </button>
-        </div>
-      </header>
-
       <main class="h_search_body">
-        <div class="h_search_input_card">
-          <Icon class="h_search_input_icon" icon="lucide:sparkles" />
+        <div class="h_search_input_card" @mousedown.left="startDragWindow">
+          <Icon class="h_search_input_icon" icon="lucide:search" />
           <input
             ref="inputRef"
             v-model="keyword"
             class="h_search_input"
             placeholder="输入关键词搜索笔记"
+            @mousedown.stop
             @keydown.enter="selectActiveSuggestion"
             @keydown.escape="closeSearch"
             @keydown.up.prevent="moveActiveSuggestion(-1)"
             @keydown.down.prevent="moveActiveSuggestion(1)"
           />
           <span class="h_search_shortcut">Enter 打开</span>
+          <button class="h_search_close" type="button" @mousedown.stop @click.stop="closeSearch">
+            <Icon icon="lucide:x" />
+          </button>
         </div>
 
-        <div class="h_search_meta">
+        <div v-if="hasKeyword" class="h_search_meta">
           <span v-if="searching">正在搜索…</span>
           <span v-else-if="hasSuggestions">找到 {{ suggestions.length }} 条结果</span>
-          <span v-else-if="hasKeyword">未找到匹配结果</span>
+          <span v-else-if="isEmptyResult">没有匹配内容</span>
           <span v-else>支持标题、描述和正文搜索</span>
 
           <span>↑↓ 选择 · Esc 关闭</span>
         </div>
 
-        <div class="h_search_results">
-          <button
-            v-for="(suggestion, index) in suggestions"
-            :key="suggestion.id"
-            :class="[
-              'h_search_result',
-              { h_search_result_active: index === activeIndex },
-            ]"
-            type="button"
-            @mouseenter="activeIndex = index"
-            @click="selectSuggestion(suggestion)"
-          >
-            <span class="h_search_result_icon">
-              <Icon icon="lucide:file-text" />
-            </span>
-            <span class="h_search_result_content">
-              <strong>{{ suggestion.title }}</strong>
-              <em>{{ suggestion.describe || suggestion.content || "打开这条笔记" }}</em>
-            </span>
-            <span class="h_search_result_action">
-              <Icon icon="lucide:corner-down-left" />
-            </span>
-          </button>
-
-          <div v-if="isEmptyInitial" class="h_search_empty">
-            <Icon icon="lucide:scan-search" />
-            <strong>开始搜索</strong>
-            <span>输入关键词后会显示匹配的笔记。</span>
+        <div v-if="hasKeyword" class="h_search_popup">
+          <div v-if="hasSuggestions" class="h_search_results">
+            <button
+              v-for="(suggestion, index) in suggestions"
+              :key="suggestion.id"
+              :class="[
+                'h_search_result',
+                { h_search_result_active: index === activeIndex },
+              ]"
+              type="button"
+              @mouseenter="activeIndex = index"
+              @click="selectSuggestion(suggestion)"
+            >
+              <span class="h_search_result_icon">
+                <Icon icon="lucide:file-text" />
+              </span>
+              <span class="h_search_result_content">
+                <strong>{{ suggestion.title }}</strong>
+                <em>{{ suggestion.describe || suggestion.content || "打开这条笔记" }}</em>
+              </span>
+              <span class="h_search_result_action">
+                <Icon icon="lucide:corner-down-left" />
+              </span>
+            </button>
           </div>
 
           <div v-else-if="isEmptyResult" class="h_search_empty">
@@ -96,7 +78,6 @@ const {
   hasKeyword,
   hasSuggestions,
   inputRef,
-  isEmptyInitial,
   isEmptyResult,
   keyword,
   moveActiveSuggestion,
