ecdcf6caf9bdb540c89b9a6d3e8a92f955962a2c
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-21 14:32:43 +0800

    feat: add reset config api

--- src/request/apis/config.ts
+++ src/request/apis/config.ts
@@ -13,6 +13,8 @@ export const getConfig = (): Promise<AppConfig> => invoke("get_config");
 export const updateConfig = (appConfig: AppConfig): Promise<AppConfig> =>
   invoke("update_config", { appConfig });
 
+export const resetConfig = (): Promise<AppConfig> => invoke("reset_config");
+
 export const openSettingsWindow = (): Promise<void> => invoke("open_settings_window");
 
 export const closeSettingsWindow = (): Promise<void> => invoke("close_settings_window");
