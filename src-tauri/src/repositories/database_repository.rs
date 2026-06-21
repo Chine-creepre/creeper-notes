820cb64c240731b77b105979bff00bccdbac12c4
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-21 13:15:01 +0800

    feat: initialize folders table

--- src-tauri/src/repositories/database_repository.rs
+++ src-tauri/src/repositories/database_repository.rs
@@ -5,7 +5,7 @@ use rusqlite::Connection;
 
 use crate::constants::database_constants::DATABASE_FILE_NAME;
 use crate::models::app_paths::AppPaths;
-use crate::repositories::tables::notes_table;
+use crate::repositories::tables::{folders_table, notes_table};
 
 pub fn get_database_file_path(app_paths: &AppPaths) -> PathBuf {
     app_paths.database_dir.join(DATABASE_FILE_NAME)
@@ -22,6 +22,7 @@ pub fn initialize_database(app_paths: &AppPaths) -> Result<PathBuf, String> {
     let connection = Connection::open(&database_file_path)
         .map_err(|error| error.to_string())?;
 
+    folders_table::initialize(&connection)?;
     notes_table::initialize(&connection)?;
 
     Ok(database_file_path)
