91506ef9de49ccad741b0702089241101e810cb2
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-21 17:30:13 +0800

    style: align title bar with design draft

--- src/components/HWindowTitleBar/index.vue
+++ src/components/HWindowTitleBar/index.vue
@@ -9,7 +9,13 @@ const { startDragWindow } = useHWindowTitleBar();
     <div
       class="h_window_title_bar_drag"
       @mousedown.left="startDragWindow">
-      <span class="h_window_title_bar_title"> creeper-notes </span>
+      <span class="h_window_title_bar_dots">
+        <i class="h_window_title_bar_dot h_window_title_bar_dot_close"></i>
+        <i class="h_window_title_bar_dot h_window_title_bar_dot_minimize"></i>
+        <i class="h_window_title_bar_dot h_window_title_bar_dot_zoom"></i>
+      </span>
+      <span class="h_window_title_bar_title">creeper-notes · 本地笔记系统</span>
+      <span class="h_window_title_bar_shortcut">Ctrl+N</span>
     </div>
   </header>
 </template>
