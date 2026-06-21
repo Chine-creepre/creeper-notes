81af9dc737faafc18a6267e5c9b6272597ec4dc4
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-21 15:07:06 +0800

    fix: use product theme as default

--- src-tauri/src/constants/config_constants.rs
+++ src-tauri/src/constants/config_constants.rs
@@ -1,6 +1,6 @@
 pub const CONFIG_FILE_NAME: &str = "app.json";
 
-pub const DEFAULT_THEME: &str = "system";
+pub const DEFAULT_THEME: &str = "blue_cyan";
 pub const DEFAULT_TOGGLE_SHORTCUT: &str = "Ctrl+N";
 pub const DEFAULT_SEARCH_SHORTCUT: &str = "Ctrl+Shift+F";
 pub const DEFAULT_AUTO_START_ENABLED: bool = false;
