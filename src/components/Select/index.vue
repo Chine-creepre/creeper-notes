a5e5f15d6baa933d1dac67939daef849f24a1402
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-22 19:49:18 +0800

    feat: add unified select component

---
+++ src/components/Select/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <select v-model="model" class="h_select">
+    <option v-if="placeholder" value="">{{ placeholder }}</option>
+    <option v-for="option in options" :key="option.value" :value="option.value">
+      {{ option.label }}
+    </option>
+  </select>
+</template>
+
+<script setup lang="ts">
+import "./index.scss";
+
+withDefaults(
+  defineProps<{
+    options: Array<{ label: string; value: string }>;
+    placeholder?: string;
+  }>(),
+  {
+    placeholder: "",
+  },
+);
+
+const model = defineModel<string>({ default: "" });
+</script>
