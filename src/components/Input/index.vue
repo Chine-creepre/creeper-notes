ee47857b0dc20ea4e00b2bfef2b4ff87756468f0
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-22 19:48:23 +0800

    feat: add unified input component

---
+++ src/components/Input/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <input v-model="model" class="h_input" :type="type" />
+</template>
+
+<script setup lang="ts">
+import "./index.scss";
+
+withDefaults(
+  defineProps<{
+    type?: string;
+  }>(),
+  {
+    type: "text",
+  },
+);
+
+const model = defineModel<string>({ default: "" });
+</script>
