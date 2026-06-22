41b1314bf9a892ad8ccf30d36df7d912c79d1227
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-22 19:47:48 +0800

    feat: add unified button component

---
+++ src/components/Button/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <button :class="['h_button', `h_button_${variant}`, `h_button_${size}`, { h_button_block: block }]" :type="type">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+import "./index.scss";
+
+withDefaults(
+  defineProps<{
+    type?: "button" | "submit" | "reset";
+    variant?: "primary" | "secondary" | "danger" | "ghost";
+    size?: "sm" | "md";
+    block?: boolean;
+  }>(),
+  {
+    type: "button",
+    variant: "primary",
+    size: "md",
+    block: false,
+  },
+);
+</script>
