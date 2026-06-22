e391851fe41724aaec3b1a275cf366da74d88b7b
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-22 19:48:49 +0800

    feat: add unified textarea component

---
+++ src/components/Textarea/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <textarea v-model="model" class="h_textarea"></textarea>
+</template>
+
+<script setup lang="ts">
+import "./index.scss";
+
+const model = defineModel<string>({ default: "" });
+</script>
