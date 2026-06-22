70b945b09f9dbd279efe95afea1fa137c6955c96
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-22 20:04:14 +0800

    feat: mount global message component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import HMessage from "@/components/Message/index.vue";
 </script>
 
 <template>
   <RouterView></RouterView>
+  <HMessage />
 </template>
