1a64e398009c2df85e51905fde6edc48478cc78a
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-22 19:00:28 +0800

    feat: add themed icon component

---
+++ src/components/ThemedIcon/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <span :class="['h_themed_icon', { h_themed_icon_active: active }]">
+    <Icon :icon="icon" />
+  </span>
+</template>
+
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
+import "./index.scss";
+
+withDefaults(
+  defineProps<{
+    icon: string;
+    active?: boolean;
+  }>(),
+  {
+    active: false,
+  },
+);
+</script>
