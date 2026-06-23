358c93ea9b58e7da7a5a6be0d2da356f6129b9ac
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-23 22:24:57 +0800

    fix: avoid dragging titlebar on double click

--- src/components/HWindowTitleBar/hook.ts
+++ src/components/HWindowTitleBar/hook.ts
@@ -1,15 +1,17 @@
 import { invoke } from "@tauri-apps/api/core";
 
 export const useHWindowTitleBar = () => {
-  const startDragWindow = async (): Promise<void> => {
+  const startDragWindow = async (event: MouseEvent) => {
+    if (event.detail > 1) return;
+
     await invoke("start_dragging_window");
   };
 
-  const toggleFullscreen = async (): Promise<void> => {
+  const toggleFullscreen = async () => {
     await invoke("toggle_main_window_fullscreen");
   };
 
-  const closeToTray = async (): Promise<void> => {
+  const closeToTray = async () => {
     await invoke("hide_main_window");
   };
 
