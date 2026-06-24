ba7a746e73fae9d22c412864c953927e1712f3c8
Creeper <132051882+Chine-creepre@users.noreply.github.com>
2026-06-24 20:29:47 +0800

    feat: add system icon component

---
+++ src/components/SystemIcon/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import appIconSrc from "@/assets/icons/32x32.png";
+
+withDefaults(
+  defineProps<{
+    alt?: string;
+  }>(),
+  {
+    alt: "Creeper Notes",
+  },
+);
+</script>
+
+<template>
+  <img class="h_system_icon" :src="appIconSrc" :alt="alt" draggable="false" />
+</template>
+
+<style lang="scss" scoped src="./index.scss"></style>
